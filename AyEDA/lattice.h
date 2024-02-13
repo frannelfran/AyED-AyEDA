3e95dc7710450b4767f9c749fe17d0aa9b54b025
usuario <alu0101571669@ull.edu.es>
2024-02-13 20:29:23 +0000

    13/02/2024

--- AyEDA/lattice.h
+++ AyEDA/lattice.h
@@ -10,7 +10,12 @@ class Lattice {
   public:
    Lattice(const Cell&); // Constructor de la clase
 
-  
+   // Getters
+   const Cell& GetCell(const Position&) const; // Obtener la célula según su posición
+
+   // Sobrecarga de operadores
+   friend ostream& operator<<(ostream& os, const Lattice& latt);
+   
   private:
    vector<Cell> lattice_; // Conjunto de células
 };
