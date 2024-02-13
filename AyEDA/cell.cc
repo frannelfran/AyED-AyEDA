8edffd52d6c42e890ee53ba7d2f89add4cafd80e
usuario <alu0101571669@ull.edu.es>
2024-02-13 20:22:20 +0000

    13/02/2024

--- AyEDA/cell.cc
+++ AyEDA/cell.cc
@@ -13,3 +13,12 @@ Cell::Cell(const Position& posicion, const State& estado) {
   this->posicion_ = posicion;
   this->estado_ = estado;
 }
+
+/**
+ * @brief Función que devuelve el estado de la célula
+ * @return Estado de la célula
+*/
+
+State Cell::GetState() const {
+  return this->estado_;
+}
--- AyEDA/cell.h
+++ AyEDA/cell.h
@@ -23,6 +23,9 @@ class Cell {
    // Funciones
    int NextState(const Lattice&);
 
+   // Sobrecarga de operadores
+   friend ostream& operator<<(ostream& os, const Cell& cell);
+
   private:
    Position posicion_;
    State estado_;
