9a7f24258996706547d0e1d7fd4b089bf796eed9
alu0101571669 <alu0101571669@ull.edu.es>
2024-04-06 22:08:31 +0100

    06/04/2024

--- AyEDA/practica05_algoritmos_de_ordenacion/src/sortmethod/sortmethod.h
+++ AyEDA/practica05_algoritmos_de_ordenacion/src/sortmethod/sortmethod.h
@@ -247,7 +247,6 @@ template <class Key> void RadixSort<Key>::Sort() const {
 template<class Key> void RadixSort<Key>::CountSort(Sequence<Key>* secuencia, int exp) const {
   int size = secuencia->GetSize();
   vector<Key> output(size);
-  Sequence<Key>* aux = new StaticSequence<Key>(size);
   int count[10] = {0};
   for (int i = 0; i < size; i++) {
     count[(secuencia->operator[](Position(i)).GetKey() / exp) % 10]++;
